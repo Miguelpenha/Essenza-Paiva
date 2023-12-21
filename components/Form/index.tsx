d691698a7f6ca79d230981785d0201a7a56184f9
Miguel Penha <miguel.penha1@gmail.com>
2023-12-20 22:20:02 -0300

    Update index.tsx

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Container, Text, Input, InputMask, Button } from './style'
 import page from '../../services/page'
 
